f06cfb180a94a70ab5f7ea5b8ee6791fdffe1ab8
志成 chen <michael112246@yahoo.com.tw>
2024-05-29 10:51:56 +0800

    MVC ViewData

---
+++ jQuery_AJAX_WebAPI_MVC/jQuery_AJAX_WebAPI_MVC/Controllers/AJAXAPIController.cs
@@ -0,0 +1,17 @@
+﻿using jQuery_AJAX_WebAPI_MVC.Models;
+using System;
+using System.Web.Http;
+
+namespace jQuery_AJAX_WebAPI_MVC.Controllers
+{
+    public class AjaxAPIController : ApiController
+    {
+        [Route("api/AjaxAPI/AjaxMethod")]
+        [HttpPost]
+        public PersonModel AjaxMethod(PersonModel person)
+        {
+            person.DateTime = DateTime.Now.ToString();
+            return person;
+        }
+    }
+}
